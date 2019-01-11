39c1f9cfebab95989532d5efb8ce517a5a128934
keyne.kassapa <k.oei@uqconnect.edu.au>
2019-01-11 11:21:28 +1000

    Update String Exercise

--- HelloWorld/hello-world.go
+++ HelloWorld/hello-world.go
@@ -8,17 +8,43 @@ import "fmt"
 // Declare the type
 type fn func(string)
 
+const author string = "Keyne Oei"
+
 // Init Function
 func main() {
 	fmt.Printf("Hello, World\n")
+
   CallOtherFunction(printHelloWorld, "Keyne")
+
+  StringEx()
+
+  printAuthor()
 }
 
+// Function Exercise
 func printHelloWorld(name string) {
-  fmt.Printf("Hello, " + name + "\n")
+  var sentence string = "Hello, " + name + "\n"
+  /*
+  OR
+  sentence := "Hello, " + name + "\n"
+  no type is specified when using :=
+  */
+  fmt.Printf(sentence)
 }
 
 // Parameter of the function must be followed by the type
 func CallOtherFunction(f1 fn, name string) {
   f1(name)
+}
+
+// Testing Constant Variable
+func printAuthor() {
+  fmt.Printf("\nAuthor --- " + author + "\n")
+}
+
+// String Exercise
+func StringEx() {
+  fmt.Println(len("Hello World")) // Print the length of the string
+  fmt.Println("Hello World"[1]) // Print decimal of "e"
+  fmt.Println("Hello " + "World")
 }
