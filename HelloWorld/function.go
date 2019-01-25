7a5b6445c41162bf29c959555431a0ce9cef6e54
keyne.kassapa <k.oei@uqconnect.edu.au>
2019-01-25 14:02:43 +1000

    Update function

--- HelloWorld/function.go
+++ HelloWorld/function.go
@@ -3,7 +3,11 @@ package main
 
 // Formatting for input and output
 // "fmt" string literal -> type of expressions
-import "fmt"
+// strconv turn integer to string
+import (
+  "strconv"
+  "fmt"
+)
 
 func main() {
   xs := []float64{98,93,77,82,83}
@@ -12,6 +16,8 @@ func main() {
   x, y := f()
   fmt.Println(x)
   fmt.Println(y)
+  // turn integer to string
+  fmt.Println("Add -- "+ strconv.Itoa(add(10, 20, 30)))
 }
 
 // parameter of float64
@@ -28,4 +34,16 @@ func average(xs []float64) float64 {
 // return two integers
 func f() (int, int) {
   return 5, 6
+}
+
+// many parameters as input
+func add(args ...int) int {
+  fmt.Println("asdf")
+  total := 0
+  // _ would be i++
+  // v would be each element of args
+  for _, v := range args {
+   total += v
+  }
+  return total
 }
