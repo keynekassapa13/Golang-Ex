f9863643bb3eeae0ac24ffd52c4f1faea812ac85
keyne.kassapa <k.oei@uqconnect.edu.au>
2019-01-26 20:06:06 +1000

    cleaning projects folder

---
+++ tutorial/interfaces.go
@@ -0,0 +1,10 @@
+// Package Declaration
+package main
+
+// Formatting for input and output
+// "fmt" string literal -> type of expressions
+// strconv turn integer to string
+import (
+  "strconv"
+  "fmt"
+)
