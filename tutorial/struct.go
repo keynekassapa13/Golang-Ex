130a53ed8e01c96eb0cdcdc7d3d7bf963d47fee9
keyne.kassapa <k.oei@uqconnect.edu.au>
2019-01-26 20:23:55 +1000

    Interface implementation

--- tutorial/struct.go
+++ tutorial/struct.go
@@ -11,8 +11,20 @@ import (
 func main()  {
   c := Circle{x: 3, y: 4, r: 5}
   fmt.Println(c.x, c.y, c.r)
+
+  c_2 := Circle{x: 3, y: 4, r: 5}
+  fmt.Println(c_2.x, c_2.y, c_2.r)
+
+  // * and & as the pointer
+  // Call the struct
   fmt.Println(circlePoint(&c))
+
+  // function in struct
   fmt.Println(c.area())
+
+  // call other function in struct
+  // interface implementation
+  fmt.Println(totalArea(&c, &c_2))
 }
 
 type Circle struct {
@@ -25,4 +37,17 @@ func circlePoint(c *Circle) float64 {
 
 func (c *Circle) area() float64 {
   return math.Pi * c.r * c.r
+}
+
+// Interface
+type Shapes interface {
+  area() float64
+}
+
+func totalArea(shapes ...Shapes) float64 {
+  var area float64
+  for _, s := range shapes {
+    area += s.area()
+  }
+  return area
 }
